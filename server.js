36e3ca47a97787c15c6dadb78781fc1e6f3e8f4d
PraveenMatheesha <77117024+Praveen-47@users.noreply.github.com>
2022-10-24 18:56:44 +0530

    connection to mongo db

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 import express from "express";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import logger from "./helpers/logger.js";
+dotenv.config();
 const app = express();
 
+const DB_URL = process.env.MONGO_DB_URL;
 
-app.use("/", require("./routes/user.routes"));
+const connect = () => {
+  mongoose.connect(DB_URL, () => {
+    logger.info(`Mongo DB Connected 😀`);
+  });
+};
 
 app.listen(8000, () => {
   console.log("app is running");
-  //   connectDB();
+  connect();
 });
